ea16581019b45169a7f9041d5da6dc8964de44cf
AAROHI DIXIT <148526981+aarohidixit31@users.noreply.github.com>
2026-01-04 00:34:10 +0530

    Add permutation generation using backtracking
    Implement backtracking algorithm to generate permutations of a string.

--- Backtracking/Tom And Permutations.java
+++ Backtracking/Tom And Permutations.java
@@ -1,1 +1,25 @@
+class Solve{
+  ArrayList<String> permute(String str){
+    ArrayList<String> result = new ArrayList<>();
+    char[] chars = str.toCharArray();
+    Arrays.sort();
 
+  boolean[] used = new boolean[chars.length];
+    backtrack(chars, new StringBuilder(), used, result);
+     return result;
+  }
+  private void backtrack(char[] chars , StringBuilder current , boolean[] used, ArrayList<String> result{
+    if(current.length()==chars.length){
+      result.add(current.toString());
+      return ;
+    }
+    for(int i=0;i<chars.length;i++){
+      if(used[i]) continue ;
+        used[i] = true;
+            current.append(chars[i]);
+            backtrack(chars, current, used, result);
+            current.deleteCharAt(current.length() - 1);
+            used[i] = false;
+        }
+    }
+}
