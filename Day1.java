734a496ad716abd642e35f750c328572a7bf11cc
AAROHI DIXIT <148526981+aarohidixit31@users.noreply.github.com>
2026-01-03 15:01:02 +0530

    Implement isHappy method to check happy numbers

---
+++ Day1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> seen = new HashSet<>();
+        
+        while (n != 1) {
+            if (seen.contains(n)) {
+                return false; // cycle detected
+            }
+            seen.add(n);
+            n = getSumOfSquares(n);
+        }
+        
+        return true;
+    }
+    
+    private int getSumOfSquares(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+}
